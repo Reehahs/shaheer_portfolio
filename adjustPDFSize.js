d49085343237da032a28bad16757ff560db4d049
Reehahs <shaheerkhan123p@gmail.com>
2025-02-14 02:01:21 -0500

    trying this again for phone lol

--- adjustPDFSize.js
+++ adjustPDFSize.js
@@ -5,8 +5,8 @@ function adjustPDFSize() {
     let aspectRatio = screenWidth / screenHeight;
 
     if (aspectRatio < 0.75) { // Portrait Mode (more height than width)
-        iframe.style.height = "90vh";
-        iframe.style.width = "100%"; // Reduce width for better fit
+        iframe.style.height = "100vh";
+        iframe.style.width = "90vw"; // Reduce width for better fit
     } else { // Landscape Mode (more width than height)
         iframe.style.height = "100vh"; // Slightly smaller to ensure it fits
         iframe.style.width = "80%";
